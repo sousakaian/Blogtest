b57e9a478b1e40d2b456635a3f54f1f0c846cad3
sousakaian <sousa.kaian@hotmail.com>
2018-06-12 13:50:13 -0300

    EXTRA

--- css/code.js
+++ css/code.js
@@ -1,158 +1,134 @@
-function myFunction() {
-	//Navigation Elements
-	let SM = document.getElementById("SM");
-    let Smim = document.getElementById("Smim");
-    let PJ = document.getElementById("PJ");
-    let Proj = document.getElementById("Proj");
-    let GL = document.getElementById("GL");
-    let Gale = document.getElementById("Gale");
-    if (Smim.checked != false) {
-        SM.style.display = "block";
-        SM.style.opacity = "1";
-        SM.style.height = "auto";
-    } else {
-        SM.style.display = "none";
-        SM.style.opacity = "0";
-        SM.style.height = "0px";
+
+function inptrue1(but) {
+    let Smim = $("#Smim");
+    let Proj = $("#Proj");
+    let Gale = $("#Gale");
+    Smim.attr('check',"false");
+    Gale.attr('check',"false");
+    Proj.attr('check',"false");
+    if(but=="Sbut"){
+        Smim.attr('check',"checked");
     }
-    if (Proj.checked != false) {
-        PJ.style.display = "block";
-        PJ.style.opacity = "1";
-        PJ.style.height = "auto";
-    } else {
-        PJ.style.display = "none";
-        PJ.style.opacity = "0";
-        PJ.style.height = "0px";
+    if(but=="Gbut"){
+        Gale.attr('check',"checked");
     }
-    if (Gale.checked != false) {
-        GL.style.display = "block";
-        GL.style.opacity = "1";
-        GL.style.height = "auto";
-    } else {
-        GL.style.display = "none";
-        GL.style.opacity = "0";
-        GL.style.height = "0px";
-    }
-    //Galery elements
-    let GL1 = document.getElementById("GL1");
-    let GL_1 = document.getElementById("GL_1");
-    let GL2 = document.getElementById("GL2");
-    let GL_2 = document.getElementById("GL_2");
-    let GL3 = document.getElementById("GL3");
-    let GL_3 = document.getElementById("GL_3");
-    let GL4 = document.getElementById("GL4");
-    let GL_4 = document.getElementById("GL_4");
-    let GL5 = document.getElementById("GL5");
-    let GL_5 = document.getElementById("GL_5");
-    if (GL1.checked != false) {
-        GL_1.style.display = "inline-block";
-        GL_1.style.opacity = "1";
-        GL_1.style.height = "auto";
-    } else {
-        GL_1.style.display = "none";
-        GL_1.style.opacity = "0";
-        GL_1.style.height = "0px";
-    }
-    if (GL2.checked != false) {
-        GL_2.style.display = "inline-block";
-        GL_2.style.opacity = "1";
-        GL_2.style.height = "auto";
-    } else {
-        GL_2.style.display = "none";
-        GL_2.style.opacity = "0";
-        GL_2.style.height = "0px";
-    }
-    if (GL3.checked != false) {
-        GL_3.style.display = "inline-block";
-        GL_3.style.opacity = "1";
-        GL_3.style.height = "auto";
-    } else {
-        GL_3.style.display = "none";
-        GL_3.style.opacity = "0";
-        GL_3.style.height = "0px";
-    }
-    if (GL4.checked != false) {
-        GL_4.style.display = "inline-block";
-        GL_4.style.opacity = "1";
-        GL_4.style.height = "auto";
-    } else {
-        GL_4.style.display = "none";
-        GL_4.style.opacity = "0";
-        GL_4.style.height = "0px";
-    }
-    if (GL5.checked != false) {
-        GL_5.style.display = "inline-block";
-        GL_5.style.opacity = "1";
-        GL_5.style.height = "auto";
-    } else {
-        GL_5.style.display = "none";
-        GL_5.style.opacity = "0";
-        GL_5.style.height = "0px";
-    }
-    //Galery elements
-    let PJ1 = document.getElementById("PJ1");
-    let PJ_1 = document.getElementById("PJ_1");
-    let PJ2 = document.getElementById("PJ2");
-    let PJ_2 = document.getElementById("PJ_2");
-    let PJ3 = document.getElementById("PJ3");
-    let PJ_3 = document.getElementById("PJ_3");
-    let PJ4 = document.getElementById("PJ4");
-    let PJ_4 = document.getElementById("PJ_4");
-    let PJ5 = document.getElementById("PJ5");
-    let PJ_5 = document.getElementById("PJ_5");
-    if (PJ1.checked != false) {
-        PJ_1.style.display = "inline-block";
-        PJ_1.style.opacity = "1";
-        PJ_1.style.height = "auto";
-    } else {
-        PJ_1.style.display = "none";
-        PJ_1.style.opacity = "0";
-        PJ_1.style.height = "0px";
-    }
-    if (PJ2.checked != false) {
-        PJ_2.style.display = "inline-block";
-        PJ_2.style.opacity = "1";
-        PJ_2.style.height = "auto";
-    } else {
-        PJ_2.style.display = "none";
-        PJ_2.style.opacity = "0";
-        PJ_2.style.height = "0px";
-    }
-    if (PJ3.checked != false) {
-        PJ_3.style.display = "inline-block";
-        PJ_3.style.opacity = "1";
-        PJ_3.style.height = "auto";
-    } else {
-        PJ_3.style.display = "none";
-        PJ_3.style.opacity = "0";
-        PJ_3.style.height = "0px";
-    }
-    if (PJ4.checked != false) {
-        PJ_4.style.display = "inline-block";
-        PJ_4.style.opacity = "1";
-        PJ_4.style.height = "auto";
-    } else {
-        PJ_4.style.display = "none";
-        PJ_4.style.opacity = "0";
-        PJ_4.style.height = "0px";
-    }
-    if (PJ5.checked != false) {
-        PJ_5.style.display = "inline-block";
-        PJ_5.style.opacity = "1";
-        PJ_5.style.height = "auto";
-    } else {
-        PJ_5.style.display = "none";
-        PJ_5.style.opacity = "0";
-        PJ_5.style.height = "0px";
+    if(but=="Pbut"){
+        Proj.attr('check',"checked");
     }
 }
+
 function glclose(){
-	var ele = document.getElementsByName("gal");
-	for(var i=0;i<ele.length;i++)
-    ele[i].checked = false;
+    var Gla = []
+    for (let i = 0; i <= $(':input[name*="gal"]').length; i++) {
+        Gla[i] = $("#GL" + i);
+        Gla[i].attr('check',"false");
+    }
 }
 function pjclose(){
-	var ele = document.getElementsByName("pro");
-    for(var i=0;i<ele.length;i++)
-	ele[i].checked = false;
+    var Pja = []
+    for (let i = 0; i <= $(':input[name*="pro"]').length; i++) {
+        Pja[i] = $("#PJ" + i);
+        Pja[i].attr('check',"false");
+    }
+}
+
+function inpt2(but) {
+    var Gla = []
+    for (let i = 0; i <= $(':input[name*="gal"]').length; i++) {
+        Gla[i] = $("#GL" + i);
+        Gla[i].attr('check',"false");
+        if(but==i){
+            Gla[i].attr('check',"checked");
+        }
+    }
+}
+function inpt3(but) {
+    var Pja = []
+    for (let i = 0; i <= $(':input[name*="pro"]').length; i++) {
+        Pja[i] = $("#PJ" + i);
+        Pja[i].attr('check',"false");
+        if(but==i){
+            Pja[i].attr('check',"checked");
+        }
+    }
+}
+
+
+function myFunction(){
+    let SM = $("#SM");
+    let Smim = $("#Smim");
+    let PJ = $("#PJ");
+    let Proj = $("#Proj");
+    let GL = $("#GL");
+    let Gale = $("#Gale"); 
+    console.log(SM.css("display"))
+    if (Smim.attr('check') == "checked") {
+        SM.css("display", "block");
+        SM.css("opacity", "1");
+        SM.css("height","auto");
+    } else {
+        SM.css("display","none");
+        SM.css("opacity","0");
+        SM.css("height","0px");
+    }
+    if (Proj.attr('check') == "checked") {
+        PJ.css("display","block");
+        PJ.css("opacity","1");
+        PJ.css("height","auto");
+    } else {
+        PJ.css("display","none");
+        PJ.css("opacity","0");
+        PJ.css("height","0px");
+    }
+    if (Gale.attr('check') == "checked") {
+        GL.css("display","block");
+        GL.css("opacity","1");
+        GL.css("height","auto");
+    } else {
+        GL.css("display","none");
+        GL.css("opacity","0");
+        GL.css("height","0px");
+    }
+    console.log(Smim.attr('check'),"2")
+    console.log(Gale.attr('check'),"2")
+    console.log(Proj.attr('check'),"2")
+    var GLa = []
+    var GL_a = []
+    var BGLa = []
+    for (let i = 0; i <= $(':input[name*="gal"]').length; i++) {
+        GLa[i] = $("#GL" + i);
+        GL_a[i] = $("#GL_" + i);
+        BGLa[i] = $("#BGL" + i);
+        if(GLa[i].attr('check') == "checked"){
+            GL_a[i].css("display","inline-block");
+            GL_a[i].css("opacity","1");
+            GL_a[i].css("height","auto");
+            BGLa[i].css("border","solid 10px black");
+        } else {
+            GL_a[i].css("display","none");
+            GL_a[i].css("opacity","0");
+            GL_a[i].css("height","0px");
+            BGLa[i].css("border","solid 10px #fa5");
+        }
+    }
+    var Pja = []
+    var Pj_a = []
+    var BPja = []
+    for (let o = 0; o <= $(':input[name*="pro"]').length; o++) {
+        Pja[o] = $("#PJ" + o);
+        Pj_a[o] = $("#PJ_" + o);
+        BPja[o] = $("#BPjaPJ" + o);
+        if(Pja[o].attr('check') == "checked"){
+            Pj_a[o].css("display","inline-block");
+            Pj_a[o].css("opacity","1");
+            Pj_a[o].css("height","auto");
+            BPja[o].css("border","solid 10px black");
+
+        } else {
+            Pj_a[o].css("display","none");
+            Pj_a[o].css("opacity","0");
+            Pj_a[o].css("height","0px");
+            BPja[o].css("border","solid 10px #fa5");
+        }
+    }
 }
