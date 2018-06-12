86fd331a7e43d29b0ffd72cbf98905994a255a45
sousakaian <38764416+sousakaian@users.noreply.github.com>
2018-06-12 17:24:08 -0300

    Add files via upload

--- css/code.js
+++ css/code.js
@@ -55,81 +55,62 @@ function inpt3(but) {
 }
 
 
-function myFunction(){
+function myFunction(z){
+	if(z>=3){return;}
+	z++
     let SM = $("#SM");
     let Smim = $("#Smim");
     let PJ = $("#PJ");
     let Proj = $("#Proj");
     let GL = $("#GL");
     let Gale = $("#Gale"); 
-    console.log(SM.css("display"))
     if (Smim.attr('check') == "checked") {
-        SM.show(1000);
-        SM.css("opacity", "1");
-        SM.css("height","auto");
+        SM.show(250);
     } else {
-        SM.hide(1000);
-        SM.css("opacity","0");
-        SM.css("height","0px");
+        SM.hide(250);
     }
     if (Proj.attr('check') == "checked") {
-        PJ.show(1000);
-        PJ.css("opacity","1");
-        PJ.css("height","auto");
+        PJ.show(250);
     } else {
-        PJ.hide(1000);
-        PJ.css("opacity","0");
-        PJ.css("height","0px");
+        PJ.hide(250);
     }
     if (Gale.attr('check') == "checked") {
-        GL.show(1000);
-        GL.css("opacity","1");
-        GL.css("height","auto");
+        GL.show(250);
     } else {
-        GL.hide(1000);
-        GL.css("opacity","0");
-        GL.css("height","0px");
+        GL.hide(250);
     }
-    console.log(Smim.attr('check'),"2")
-    console.log(Gale.attr('check'),"2")
-    console.log(Proj.attr('check'),"2")
-    var GLa = []
-    var GL_a = []
-    var BGLa = []
     for (let i = 0; i <= $(':input[name*="gal"]').length; i++) {
-        GLa[i] = $("#GL" + i);
-        GL_a[i] = $("#GL_" + i);
-        BGLa[i] = $("#BGL" + i);
-        if(GLa[i].attr('check') == "checked"){
-            GL_a[i].show(1000);
-            GL_a[i].css("opacity","1");
-            GL_a[i].css("height","auto");
-            BGLa[i].css("border","solid 10px black");
+        console.log($("#GL" + i).attr('check'),i)
+        if($("#GL" + i).attr('check') == "checked"){
+            GLs(i);
         } else {
-            GL_a[i].hide(1000);
-            GL_a[i].css("opacity","0");
-            GL_a[i].css("height","0px");
-            BGLa[i].css("border","solid 10px #fa5");
+            GLh(i);
         }
     }
-    var Pja = []
-    var Pj_a = []
-    var BPja = []
     for (let o = 0; o <= $(':input[name*="pro"]').length; o++) {
-        Pja[o] = $("#PJ" + o);
-        Pj_a[o] = $("#PJ_" + o);
-        BPja[o] = $("#BPjaPJ" + o);
-        if(Pja[o].attr('check') == "checked"){
-            Pj_a[o].show(1000);
-            Pj_a[o].css("opacity","1");
-            Pj_a[o].css("height","auto");
-            BPja[o].css("border","solid 10px black");
-
+        console.log($("#PJ" + o).attr('check'),o)
+        if($("#PJ" + o).attr('check') == "checked"){
+            PJs(o);
         } else {
-            Pj_a[o].hide(1000);
-            Pj_a[o].css("opacity","0");
-            Pj_a[o].css("height","0px");
-            BPja[o].css("border","solid 10px #fa5");
+            PJh(o);
         }
     }
+    myFunction(z);
+}
+
+function GLs(a){
+    $("#GL_" + a).show(500);
+    $("#BGL" + a).css("border","solid 10px black");
+}
+function GLh(a){
+    $("#GL_" + a).hide(500);
+    $("#BGL" + a).css("border","solid 10px #fa5");
+}
+function PJs(a){
+    $("#PJ_" + a).show(500);
+    $("#BPJ" + a).css("border","solid 10px black");
+}
+function PJh(a){
+    $("#PJ_" + a).hide(500);
+    $("#BPJ" + a).css("border","solid 10px #fa5");
 }
